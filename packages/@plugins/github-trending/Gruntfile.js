aa6897d28663151e47e2fb04f32807dcb0ced8d7
upcwangying <upcwangying@126.com>
2020-01-05 20:28:40 +0800

    feat: add grunt

---
+++ packages/@plugins/github-trending/Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: require('./package.json'),
+    manifest: require('./manifest.json'),
+
+    crx: {
+      staging: {
+        src: ['src/**/*', '!.{git,svn}', '!*.pem'],
+        dest: 'dist/staging/<%= pkg.name %>-<%= manifest.version %>-dev.crx',
+      },
+      production: {
+        files: {
+          'dist/production/<%= pkg.name %>-<%= manifest.version %>-dev.crx': [
+            'src/**/*',
+            '!.{git,svn}',
+            '!*.pem',
+            '!dev/**',
+          ],
+          'dist/production/<%= pkg.name %>-<%= manifest.version %>-dev.zip': [
+            'src/**/*',
+            '!.{git,svn}',
+            '!*.pem',
+            '!dev/**',
+          ],
+        },
+      },
+    },
+  })
+
+  grunt.loadNpmTasks('grunt-crx')
+  grunt.registerTask('default', ['grunt-crx'])
+}
