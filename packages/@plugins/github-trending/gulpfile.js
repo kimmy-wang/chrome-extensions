f05637deba243ea765b55ac9f86ec42db82dc892
upcwangying <upcwangying@126.com>
2020-01-06 10:15:03 +0800

    fix: update dependencies

--- packages/@plugins/github-trending/gulpfile.js
+++ packages/@plugins/github-trending/gulpfile.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var path = require('path')
 var gulp = require('gulp')
 var crx = require('@crxs/gulp-crx')
 var manifest = require('./src/manifest.json')
@@ -8,7 +9,7 @@ gulp.task('crx', function() {
     .src('./src')
     .pipe(
       crx({
-        privateKey: fs.readFileSync('./key.pem', 'utf8'),
+        privateKey: fs.readFileSync(path.join(__dirname, '../../../certs/key.pem'), 'utf8'),
         filename: manifest.name + '-' + manifest.version + '.crx',
       }),
     )
